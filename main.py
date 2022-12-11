65b078360836158468eaa678a1b4e2fb07153d12
poojaagrawal412 <agrawalp431@gmail.com>
2022-12-11 16:22:15 +0530

    commiting db

--- main.py
+++ main.py
@@ -89,4 +89,6 @@ def main():
 if __name__=="__main__":
     #main()
     # set_env_variable(env_file_path)
-    app_run(app, host=APP_HOST, port=APP_PORT)
+  #  app_run(app, host=APP_HOST, port=APP_PORT)
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
