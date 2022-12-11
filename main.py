88d61b18af822556f682d974cca2060a6e53b83a
poojaagrawal412 <agrawalp431@gmail.com>
2022-12-11 23:16:19 +0530

    commiting main

--- main.py
+++ main.py
@@ -89,6 +89,4 @@ def main():
 if __name__=="__main__":
     #main()
     # set_env_variable(env_file_path)
-  #  app_run(app, host=APP_HOST, port=APP_PORT)
-        training_pipeline = TrainPipeline()
-        training_pipeline.run_pipeline()
+   app_run(app, host=APP_HOST, port=APP_PORT)
--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -2,7 +2,7 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import  train_test_split
 import os,sys
 from pandas import DataFrame
 from sensor.data_access.sensor_data import SensorData
